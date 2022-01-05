824fdd253aec952500e8f5a35ab0a6d9337126e3
Stanisław Kucznerowicz <46819190+stasiu9@users.noreply.github.com>
2022-01-05 20:43:56 +0100

    Added header

--- Frontend/AnimalShelter/src/app/navigation/navigation.component.ts
+++ Frontend/AnimalShelter/src/app/navigation/navigation.component.ts
@@ -1,20 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { filter, map, shareReplay } from 'rxjs/operators';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss'],
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
   isHandset$: Observable<boolean> = this.breakpointObserver
     .observe(Breakpoints.Handset)
     .pipe(
       map((result) => result.matches),
       shareReplay()
     );
+  subTitle: string = '';
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private router: Router
+  ) {
+    router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event) => {
+        if (event instanceof NavigationEnd) {
+          console.log(event.url);
+          switch (event.url) {
+            case '/home': {
+              this.subTitle = 'Home';
+              break;
+            }
+            case '/about': {
+              this.subTitle = 'About';
+              break;
+            }
+            case '/adoptions': {
+              this.subTitle = 'Adoptions';
+              break;
+            }
+            case '/fundraisers': {
+              this.subTitle = 'Fundraisers';
+              break;
+            }
+            case '/user': {
+              this.subTitle = 'Users';
+              break;
+            }
+          }
+        }
+      });
+  }
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  ngOnInit() {}
 }
